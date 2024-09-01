efbc6090914d1ac939453b05b7cf4612d44d4eed
Ahmad Vafaeian <64265676+advafaeian@users.noreply.github.com>
2024-09-01 13:53:14 +0330

    Update local.sh
    Adapting to the Username Change

--- local.sh
+++ local.sh
@@ -2,6 +2,6 @@
 read -p "Remote Machine IP Address": ip
 read -p "Project Location": location
 
-ssh root@$ip "bash <(curl -Ls https://raw.githubusercontent.com/humahn/rstudio-auto-setup/main/remote.sh)"
+ssh root@$ip "bash <(curl -Ls https://raw.githubusercontent.com/advafaeian/rstudio-auto-setup/main/remote.sh)"
 
 scp -r "$location" hmn@$ip:~
